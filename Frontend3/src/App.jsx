a996c5b45b355819da86c396fd08418d2fcb2a4f
muhammad-noman732 <iasknoman156@gmail.com>
2025-11-15 23:29:45 -0800

    Feat: remove the auth logic from the frontend part

--- Frontend3/src/App.jsx
+++ Frontend3/src/App.jsx
@@ -28,57 +28,78 @@ function App() {
 
   return (
     <div>
-      {userData && <Nav/>}
+      {/* Auth-guarded Nav: {userData && <Nav/>} */}
+      <Nav/>
 
          <ToastContainer />
 
       <Routes>
 
-
+      {/* Guarded:
       <Route path='/signup' element={userData ? 
         (<Navigate  to={location.state?.from || "/" } />) 
-        :(<Registeration/>)} />
+        :(<Registeration/>)} /> */}
+      <Route path='/signup' element={<Registeration/>} />
 
      
+       {/* Guarded:
        <Route path='/login' element={userData ? 
         (<Navigate  to={location.state?.from || "/" } />) 
-        :(<Login/>)}/>   
+        :(<Login/>)}/>    */}
+       <Route path='/login' element={<Login/>}/>   
    
-   
-     <Route path='/' element={userData ? <Home/> : <Navigate  to='/login' state={{from:location.pathname}}/>} />   
+     {/* Guarded:
+     <Route path='/' element={userData ? <Home/> : <Navigate  to='/login' state={{from:location.pathname}}/>} />    */}
+     <Route path='/' element={<Home/>} />   
 
 
 
         
        
        
+     {/* Guarded:
      <Route path='/about' element={userData ? <About/> : <Navigate  to='/login' state={{from:location.pathname}}/>}
-       />
+       /> */}
+     <Route path='/about' element={<About/>} />
 
      
+     {/* Guarded:
      <Route path='/collection' element={userData ? <Collections/> : <Navigate  to='/login' state={{from:location.pathname}}/>}
-       />
+       /> */}
+     <Route path='/collection' element={<Collections/>} />
 
      
+     {/* Guarded:
      <Route path='/product' element={userData ? <Product/> : <Navigate  to='/login' state={{from:location.pathname}}/>}
-       /> 
+       />  */}
+     <Route path='/product' element={<Product/>} /> 
 
      
+     {/* Guarded:
      <Route path='/contact' element={userData ? <Contact/> : <Navigate  to='/login' state={{from:location.pathname}}/>}
-       />  
+       />   */}
+     <Route path='/contact' element={<Contact/>} />  
 
+     {/* Guarded:
      <Route path='/productdetail/:productId' element={userData ? <ProductDetailts/> : <Navigate  to='/login' state={{from:location.pathname}}/>}
-       />        
+       />         */}
+     <Route path='/productdetail/:productId' element={<ProductDetailts/>} />        
 
+     {/* Guarded:
      <Route path='/cart' element={userData ? <Cart/> : <Navigate  to='/login' state={{from:location.pathname}}/>}
-       />
+       /> */}
+     <Route path='/cart' element={<Cart/>} />
 
+     {/* Guarded:
      <Route path='/placeorder' element={userData ? <PlaceOrder/> : <Navigate  to='/login' state={{from:location.pathname}}/>}
-       /> 
+       />  */}
+     <Route path='/placeorder' element={<PlaceOrder/>} /> 
 
 
+     {/* Guarded:
      <Route path='/order' element={userData ? <Order/> : <Navigate  to='/login' state={{from:location.pathname}}/>}
-       />              
+       />               */}
+     <Route path='/order' element={<Order/>} />              
 
 
       <Route path='*' element={<NotFound/>}/>
